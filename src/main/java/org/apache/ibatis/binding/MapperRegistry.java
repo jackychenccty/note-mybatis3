1ad41383d124aaecc68a7867ba5840f9a8f3130d
jackychenccty <229147258@qq.com>
2020-09-16 17:12:06 +0800

    在解析mapper映射器时，封装成了mapperstatement

--- src/main/java/org/apache/ibatis/binding/MapperRegistry.java
+++ src/main/java/org/apache/ibatis/binding/MapperRegistry.java
@@ -69,6 +69,7 @@ public class MapperRegistry {
         // otherwise the binding may automatically be attempted by the
         // mapper parser. If the type is already known, it won't try.
         MapperAnnotationBuilder parser = new MapperAnnotationBuilder(config, type);
+        // 封装映射器为MappedStatement对象
         parser.parse();
         loadCompleted = true;
       } finally {
